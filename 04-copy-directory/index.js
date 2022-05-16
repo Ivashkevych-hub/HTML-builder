4d1066cd1ff4020d143dc2d2035f1390a2629d9c
Alexei Ivashkevych <ivashkevich1991@gmail.com>
2022-05-16 14:52:08 +0300

    feat:complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path');
+const fsp = require('fs').promises;
 
+// fs.mkdir(path.join(__dirname, 'Copy'), err => {
+//   if (err) throw err;
+//   console.log('Папка "Copy" создана');
+// });
+const output = path.join(__dirname, 'Copy') ;
+const input = path.join(__dirname,'files');
+
+
+
+async function copyDir(input,output) {
+  const entries = await fsp.readdir(input,{withFileTypes:true});
+  await fsp.mkdir(output , { recursive: true });
+  console.log('Папка "Copy" создана');
+  for(let entry of entries) {
+    const inputPath = path.join(input,entry.name);
+    const outputPath = path.join(output,entry.name);
+
+    if(entry.isDirectory()) {
+      await copyDir(inputPath,outputPath);
+    } else {
+      await fsp.copyFile(inputPath,outputPath);
+    }
+  }
+}
+
+copyDir(input,output);
